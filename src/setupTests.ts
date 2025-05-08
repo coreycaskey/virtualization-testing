856103e5b10986c77c786c6d7deb76acea6a7f2d
Corey Caskey <coreyrcaskey@gmail.com>
2025-05-08 16:38:26 -0400

    update configurations, linting, ts

---
+++ src/setupTests.ts
@@ -0,0 +1,3 @@
+// This file is automatically run before each test suite if referenced in your vite.config.ts. It’s commonly used for extending matchers or initializing test environments
+
+import "@testing-library/jest-dom"; // extends expect() with DOM-specific matchers
