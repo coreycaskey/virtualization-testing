2e406628911731b6be58cff3c28d645da0968441
Corey Caskey <coreyrcaskey@gmail.com>
2025-09-07 15:10:22 -0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/virtualization-testing",
   plugins: [
     react(),
     tsconfigPaths(),
